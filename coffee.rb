49d01926a7fc54fe8e7038cfb64baef1e7476405
CosmicCandi <kalea.wolff@gmail.com>
2017-02-06 18:33:54 -0500

    Initial commit. Explorer Mode complete/ working.

--- coffee.rb
+++ coffee.rb
@@ -1,3 +1,27 @@
 class Coffee
-  
+
+  attr_accessor :coffee_name, :caffeine_amount, :drinks_before_empty
+
+  def initialize(coffee_name, caffeine_amount = 0.31, drinks_before_empty = 3)
+    self.coffee_name = coffee_name
+    self.caffeine_amount = caffeine_amount
+    self.drinks_before_empty = drinks_before_empty
+  end
+
+  def full?
+    if drinks_before_empty == 3
+      return true
+    elsif drinks_before_empty < 3
+      return false
+    end
+  end
+
+  def empty?
+    if drinks_before_empty != 0
+      return false
+    else
+      return true
+    end
+  end
+
 end
--- human.rb
+++ human.rb
@@ -1,11 +1,31 @@
 class Human
   attr_accessor :name,
-                :alertness
+                :alertness,
+                :cuppa_joe
 
   def initialize(name, alertness=0)
     self.name = name
     self.alertness = alertness
   end
 
+  def has_coffee?
+    false
+  end
+
+  def needs_coffee?
+    true
+  end
+
+  def buy(coffee_name)
+    self.cuppa_joe = coffee_name
+  end
+
+  def drink!
+    self.alertness += cuppa_joe.caffeine_amount
+    cuppa_joe.drinks_before_empty -= 1
+  end
+
+  def <
+  end
 
 end
